caee249465713ebe8b9f1c9de121819aded7e41a
Reza Stallone <reza.stallone@dana.id>
2021-02-25 10:25:48 +0700

    add product data

--- pos/serializers.py
+++ pos/serializers.py
@@ -1,7 +1,47 @@
 from rest_framework import serializers
-from .models import Product
+from django.contrib.auth.models import User, Group
+from .models import Product, Snippet, LANGUAGE_CHOICE, STYLE_CHOICE
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
-        fields = ('description',)
+        fields = ('id', 'description', 'harga')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups', 'snippets']
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
+
+# class SnippetSerializer(serializers.Serializer):
+#     class Meta:
+#         id = serializers.IntegerField(read_only=True)
+#         title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#         code = serializers.CharField(style={'base_template': 'textarea.html'})
+#         linenos = serializers.BooleanField(required=False)
+#         language = serializers.ChoiceField(choices=LANGUAGE_CHOICE, default='python')
+#         style = serializers.ChoiceField(choices=STYLE_CHOICE, default='friendly')
+
+#         def create(self, validated_data):
+#             return Snippet.objects.create(validated_data)
+
+#         def update(self, instance, validated_data):
+#             instance.title = validated_data.get('title', instance.title)
+#             instance.code = validated_data.get('code', instance.code)
+#             instance.linenos = validated_data.get('linenos', instance.linenos)
+#             instance.language = validated_data.get('language', instance.language)
+#             instance.style = validated_data.get('style', instance.style)
+#             instance.save()
+#             return instance
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Snippet
+        owner = serializers.ReadOnlyField(source='owner.username')
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
--- pos/urls.py
+++ pos/urls.py
@@ -4,10 +4,17 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'product', views.ProductViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path('api/v1/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
