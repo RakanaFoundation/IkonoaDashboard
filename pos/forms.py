2a2029903e8ab70f41495f20df2aa309d818b4df
RakanaFoundation <abdulatief@outlook.com>
2021-07-04 12:52:06 +0700

    remove generic filter

--- pos/forms.py
+++ pos/forms.py
@@ -1,6 +1,4 @@
 from django import forms
-from django.utils.translation import ugettext_lazy as _
-from django_genericfilters import forms as gf
 
 
 class LoginForm(forms.Form):
@@ -18,18 +16,3 @@ class LoginForm(forms.Form):
                 "class": "block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
             }
         ))
-
-
-class UserListForm(gf.FilteredForm):
-    is_active = gf.ChoiceField(label=_('Status'),
-                               choices=(('yes', _('Active')),
-                                        ('no', _('Unactive'))))
-
-    is_staff = gf.ChoiceField(label=_('Staff'))
-
-    is_superuser = gf.ChoiceField(label=_('Superuser'))
-
-    def get_order_by_choices(self):
-        return [('date_joined', _(u'date joined')),
-                ('last_login', _(u'last login')),
-                ('last_name', _(u'Name'))]
